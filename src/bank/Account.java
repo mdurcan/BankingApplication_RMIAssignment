54287378b56fb8adefbe3fa62b81c9c9f7d7535a
martin durcan <m.durcan8@nuigalway.ie>
2017-02-04 18:43:35 +0000

    started account class

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -1,5 +1,62 @@
 package bank;
 
-public class Account {
+import java.util.Random;
 
+public class Account {
+	
+	// variables 
+	private String UserName;
+	private String Password;
+	private String AccountName;
+	private int AccountNum;
+	private int Balance;
+	//for getting account number
+	private static int nextAccNum=0;
+	
+	//Constructor
+	public Account(String username, String password, String accName){
+		//set details
+		UserName = username;
+		Password = password;
+		AccountName = accName;
+		
+		//assign account number
+		AccountNum = nextAccNum;
+		nextAccNum++;
+		//start balance
+		Balance = 0;
+		
+	}
+	
+	//getters
+	public String GetUserName(){
+		return UserName;
+	}
+	
+	public String GetPassword(){
+		return Password;
+	}
+	
+	public String GetAccountName(){
+		return AccountName;
+	}
+	
+	public int GetAccountNum(){
+		return AccountNum;
+	}
+	
+	public int GetBalance(){
+		return Balance;
+	}
+	
+	//Functions
+	public void Deposit(int amount){
+		Balance = Balance + amount;
+	}
+	
+	public void Withdraw(int amount){
+		Balance = Balance - amount;
+	}
+	
+	
 }
